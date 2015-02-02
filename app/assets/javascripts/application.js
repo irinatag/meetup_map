4210bd09e521a9c8ac643bd2de0d24385b274b01
Irina Tagintseva <irinatag@gmail.com>
2015-02-02 15:15:04 -0800

    add things to get semantic ui to work on heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,25 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+$(document).ready(function() {
+  $('.button').on('click', function() {
+    var value = $('#input').val()
+    console.log(value)
+    var url = "http://www.omdbapi.com/?s=" + value
+
+    var success = function(data) {
+      $('#list').empty();
+      console.log(data)
+      for (var i = 0; i < data.Search.length; i++) {
+        var title = data["Search"][i]["Title"]
+        var element =  '<a data-imdbid="'+data["Search"][i]["imdbID"]+'"><div class="content"><div class="title">' + title + '</a>'
+        console.log(element);
+        $( "#list" ).append(element);
+      }
+      //console.log($($('p')[0]).data('imdbid')); //need to wrap jquery object in parentesis b/c it comes back as just string otherwise
+    };
+    $.ajax(url, { type: 'get', dataType: 'json' }).done(success);
+  });

36c823246c08d8a88ec3900ac3004393d19389cb
Irina Tagintseva <irinatag@gmail.com>
2015-02-02 14:44:20 -0800

    new rails app; add semantic ui

---
+++ app/assets/javascripts/meetup.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() {
+//   var url = 'https://api.meetup.com/events?status=upcoming&order=time&limited_events=False&desc=false&offset=0&photo-host=public&format=json&page=20&fields=&sig_id=10653533&sig=594ee8c4bb58d735c6ab32c828210391680658e5&member_id=' + '10653533'
+//   $.ajax(url, { type: 'get', dataType: 'json' }).done( function(data) {
+//      console.log(data)
+//   });
+//
+// });
+
+$(document).ready(function(){
+  $.getJSON("http://api.meetup.com/2/open_events.json?zip=10001&radius=7&topic=technology&status=upcoming&time=,1w&key=APIKEY&callback=?", function (data) {
+    console.log(data);
+    // var htmlString = "";
+    // $.each(data.results, function (i, item) {
+    });
+  });

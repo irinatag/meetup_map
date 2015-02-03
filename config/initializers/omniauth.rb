fcf09a7fb3e992ef857b66e3212134adbff0e7ac
Irina Tagintseva <irinatag@gmail.com>
2015-02-02 17:14:19 -0800

    install dotenv gem

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :meetup, ENV['j3h4jsh16js9d5f9ef881p1d06'], ENV['g15h3jdudnavl50jmmrrspv7tk']
+  provider :meetup, ENV['MEETUP_KEY'], ENV['MEETUP_SECRET']
 end

7ac88ac94789197d682cfa71b118d058edc46fde
Irina Tagintseva <irinatag@gmail.com>
2015-02-02 21:53:22 -0800

    new heroku app

--- app/assets/javascripts/meetup.js
+++ app/assets/javascripts/meetup.js
@@ -1,4 +1,20 @@
 // $(document).ready(function() {
+//   $.getJSON( , function( data ) {
+//     console.log(data);
+//   });
+// });
+// var items = [];
+// $.each( data, function( key, val ) {
+//   items.push( "<li id='" + key + "'>" + val + "</li>" );
+// });
+//
+// $( "<ul/>", {
+//   "class": "my-new-list",
+//   html: items.join( "" )
+// }).appendTo( "body" );
+
+
+// $(document).ready(function() {
 //   var url = 'https://api.meetup.com/events?status=upcoming&order=time&limited_events=False&desc=false&offset=0&photo-host=public&format=json&page=20&fields=&sig_id=10653533&sig=594ee8c4bb58d735c6ab32c828210391680658e5&member_id=' + '10653533'
 //   $.ajax(url, { type: 'get', dataType: 'json' }).done( function(data) {
 //      console.log(data)
@@ -6,10 +22,10 @@
 //
 // });
 
-$(document).ready(function(){
-  $.getJSON("http://api.meetup.com/2/open_events.json?zip=10001&radius=7&topic=technology&status=upcoming&time=,1w&key=APIKEY&callback=?", function (data) {
-    console.log(data);
-    // var htmlString = "";
-    // $.each(data.results, function (i, item) {
-    });
-  });
+// $(document).ready(function(){
+//   // $.getJSON("http://api.meetup.com/2/open_events.json?zip=10001&radius=7&topic=technology&status=upcoming&time=,1w&key=APIKEY&callback=?", function (data) {
+//   //   console.log(data);
+//     // var htmlString = "";
+//     // $.each(data.results, function (i, item) {
+//     });
+//   });
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
 
+  def login
+
+  end
+
+  def show
+    @data = File.read("public/assets/javascripts/events.json");
+    render json: @data
+  end
 
 end

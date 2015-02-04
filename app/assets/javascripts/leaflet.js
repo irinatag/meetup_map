c0da16ee73f6ca8b17103096e87e6070f86a4814
Irina Tagintseva <irinatag@gmail.com>
2015-02-04 11:10:23 -0800

    pins on map; remove database_authenticatable from model

--- app/assets/javascripts/leaflet.js
+++ app/assets/javascripts/leaflet.js
@@ -3,96 +3,107 @@ $(document).ready(function() {
   var map = L.map('map').setView([37.7272, -122.3226], 12);
   console.log(map);
 
-  var layer = L.tileLayer('http://{s}.tiles.mapbox.com/v4/irinatag.29e71c77/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaXJpbmF0YWciLCJhIjoiVktramt0OCJ9.ZQ9EtKOUUae2fVSGehSnBQ', {
+  var baseLayer = L.tileLayer('http://{s}.tiles.mapbox.com/v4/irinatag.29e71c77/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaXJpbmF0YWciLCJhIjoiVktramt0OCJ9.ZQ9EtKOUUae2fVSGehSnBQ', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
   });
-  console.log(layer);
 
-  layer.addTo(map);
+  baseLayer.addTo(map);
+
+  // playing with onclick to view lan log - NOT IMPORTANT
+  // var popup = L.popup();
+  //
+  // function onMapClick(e) {
+  //   popup
+  //   .setLatLng(e.latlng)
+  //   .setContent("You clicked the map at " + e.latlng.toString())
+  //   .openOn(map);
+  // }
+  //
+  // map.on('click', onMapClick);
 
   var data = {
     "results": [
-      {
-        "rsvp_limit": 45,
-        "status": "upcoming",
-        "visibility": "public",
-        "maybe_rsvp_count": 0,
-        "venue": {
-          "id": 23583739,
-          "lon": -122.39978,
-          "repinned": false,
-          "name": "Minted",
-          "state": "CA",
-          "address_1": "747 Front Street #200",
-          "lat": 37.798138,
-          "city": "San Francisco",
-          "country": "us"
-        },
-        "id": "219881802",
-        "utc_offset": -28800000,
-        "duration": 9000000,
-        "time": 1422930600000,
-        "waitlist_count": 0,
-        "updated": 1422909733000,
-        "yes_rsvp_count": 42,
-        "created": 1421440101000,
-        "event_url": "http://www.meetup.com/Women-Who-Code-SF/events/219881802/",
-        "description": "<p>Welcome to Women Who Code Mobile - San Francisco!",
-        "how_to_find_us": "Yes, there is bike parking.",
-        "name": "Mobile Study Group: iOS & Android (Objective-C & Java)",
-        "headcount": 0,
-        "group": {
-          "id": 2252591,
-          "created": 1311875189000,
-          "group_lat": 37.77000045776367,
-          "name": "Women Who Code SF",
-          "group_lon": -122.41000366210938,
-          "join_mode": "open",
-          "urlname": "Women-Who-Code-SF",
-          "who": "Coders"
-        }
+    {
+      "rsvp_limit": 45,
+      "status": "upcoming",
+      "visibility": "public",
+      "maybe_rsvp_count": 0,
+      "venue": {
+        "id": 23583739,
+        "lon": -122.39978,
+        "repinned": false,
+        "name": "Minted",
+        "state": "CA",
+        "address_1": "747 Front Street #200",
+        "lat": 37.798138,
+        "city": "San Francisco",
+        "country": "us"
       },
-      {
-        "rsvp_limit": 25,
-        "status": "upcoming",
-        "visibility": "public",
-        "maybe_rsvp_count": 0,
-        "venue": {
-          "id": 23611852,
-          "lon": -122.400833,
-          "repinned": false,
-          "name": "Boatbond",
-          "state": "CA",
-          "address_1": "435 Harriet Street",
-          "lat": 37.774044,
-          "city": "San Francisco",
-          "country": "us"
-        },
-        "id": "hnpjdlytdbdb",
-        "utc_offset": -28800000,
-        "duration": 7200000,
-        "time": 1422932400000,
-        "waitlist_count": 0,
-        "updated": 1422924998000,
-        "yes_rsvp_count": 6,
-        "created": 1419911913000,
-        "event_url": "http://www.meetup.com/Girl-Develop-It-San-Francisco/events/219706378/",
-        "description": "<p>Join us every Monday for this self-paced HTML/CSS/JavaScript/AngularJS/d3.js/Three.js study group where we come together to eat snacks, study and help each other out. TAs will be there to help you get unstuck when you can't figure out how to get those pesky error messages to go away.</p> <p><br/>If you don't already have something you're working on, here are places to practice:</p>",
-        "how_to_find_us": "Check in on the 8th floor",
-        "name": "GDI Study Group",
-        "headcount": 0,
-        "group": {
-          "id": 3286622,
-          "created": 1329147725000,
-          "group_lat": 37.77000045776367,
-          "name": "Girl Develop It San Francisco",
-          "group_lon": -122.4000015258789,
-          "join_mode": "open",
-          "urlname": "Girl-Develop-It-San-Francisco",
-          "who": "Nerdettes"
-        }
+      "id": "219881802",
+      "utc_offset": -28800000,
+      "duration": 9000000,
+      "time": 1422930600000,
+      "waitlist_count": 0,
+      "updated": 1422909733000,
+      "yes_rsvp_count": 42,
+      "created": 1421440101000,
+      "event_url": "http://www.meetup.com/Women-Who-Code-SF/events/219881802/",
+      "description": "<p>Welcome to Women Who Code Mobile - San Francisco!",
+      "how_to_find_us": "Yes, there is bike parking.",
+      "name": "Mobile Study Group: iOS & Android (Objective-C & Java)",
+      "headcount": 0,
+      "group": {
+        "id": 2252591,
+        "created": 1311875189000,
+        "group_lat": 37.77000045776367,
+        "name": "Women Who Code SF",
+        "group_lon": -122.41000366210938,
+        "join_mode": "open",
+        "urlname": "Women-Who-Code-SF",
+        "who": "Coders"
+      }
+    },
+    {
+      "rsvp_limit": 25,
+      "status": "upcoming",
+      "visibility": "public",
+      "maybe_rsvp_count": 0,
+      "venue": {
+        "id": 23611852,
+        "lon": -122.400833,
+        "repinned": false,
+        "name": "Boatbond",
+        "state": "CA",
+        "address_1": "435 Harriet Street",
+        "lat": 37.774044,
+        "city": "San Francisco",
+        "country": "us"
       },
+      "id": "hnpjdlytdbdb",
+      "utc_offset": -28800000,
+      "duration": 7200000,
+      "time": 1422932400000,
+      "waitlist_count": 0,
+      "updated": 1422924998000,
+      "yes_rsvp_count": 6,
+      "created": 1419911913000,
+      "event_url": "http://www.meetup.com/Girl-Develop-It-San-Francisco/events/219706378/",
+      "description": "<p>Join us every Monday for this self-paced HTML/CSS/JavaScript/AngularJS/d3.js/Three.js study group where we come together to eat snacks, study and help each other out. TAs will be there to help you get unstuck when you can't figure out how to get those pesky error messages to go away.</p> <p><br/>If you don't already have something you're working on, here are places to practice:</p>",
+      "how_to_find_us": "Check in on the 8th floor",
+      "name": "GDI Study Group",
+      "headcount": 0,
+      "group": {
+        "id": 3286622,
+        "created": 1329147725000,
+        "group_lat": 37.77000045776367,
+        "name": "Girl Develop It San Francisco",
+        "group_lon": -122.4000015258789,
+        "join_mode": "open",
+        "urlname": "Girl-Develop-It-San-Francisco",
+        "who": "Nerdettes"
+      }
+    },
     ],
     "meta": {
       "lon": "",
@@ -111,7 +122,15 @@ $(document).ready(function() {
     }
   };
 
+  console.log(data["results"][0]["venue"]["lat"]);
   console.log(data["results"][0]["venue"]["lon"]);
+
+  for (var i=0; i < data["results"].length; i++) {
+    var lat = data["results"][0]["venue"]["lat"];
+    var lon = data["results"][0]["venue"]["lon"];
+    var marker = L.marker([lat, lon]).addTo(map);
+    marker.bindPopup("<b>Mobile Study Group: iOS & Android (Objective-C & Java)</b><br>747 Front St.").openPopup();
+  }
 
 
 
--- app/assets/javascripts/meetup.js
+++ app/assets/javascripts/meetup.js
@@ -1,5 +1,3 @@
-
-
 // $(document).ready(function() {
 //   $.getJSON( , function( data ) {
 //     console.log(data);

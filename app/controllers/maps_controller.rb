f8dcc609cda3fa13b245fa194a66e90165fee5c9
Irina Tagintseva <irinatag@gmail.com>
2015-02-05 10:48:51 -0800

    remove self from url

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -6,7 +6,7 @@ class MapsController < ApplicationController
     puts session["devise.meetup_data"]
     @token = session["devise.meetup_data"]["credentials"]["token"]
     # @response = RestClient.get 'https://api.meetup.com/2/member/self/', {:params => {:access_token => @token}}
-    @response = RestClient.get 'https://api.meetup.com/2/events/self/', {:params => {:access_token => @token}}
+    @response = RestClient.get 'https://api.meetup.com/2/events/', {:params => {:access_token => @token}}
     puts @response
   end
 

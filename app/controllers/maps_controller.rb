7ca54391fb623757e620b533baa7a35fc5e4ab7d
Irina Tagintseva <irinatag@gmail.com>
2015-02-05 10:12:00 -0800

    remove puts statement for outh hash from login

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,11 +3,13 @@ require 'rest_client'
 class MapsController < ApplicationController
 
   def show
+    puts session["devise.meetup_data"]
+    @token = session["devise.meetup_data"]["credentials"]["token"]
+    @response = RestClient.get 'https://api.meetup.com/2/member/self/', {:params => {:access_token => @token}}
+    puts @response
   end
 
   def api_call
-    @response = RestClient.get 'https://api.meetup.com/2/member/self/', {:params => {:access_token => "2462d00548212213a467afbd2cf165ae"}}
-    @response.code
   end
 
 end

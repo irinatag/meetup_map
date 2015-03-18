97e9a92e9c4cd4005ce3360d64ff52054d67f083
Irina Tagintseva <irinatag@gmail.com>
2015-03-17 19:06:04 -0700

    debug controllers

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,10 +3,12 @@ require 'rest_client'
 class MapsController < ApplicationController
 
   def show
-    puts "*" * 50
-    puts session["devise.meetup_data"]
+    puts "session hash:" + session["devise.meetup_data"]
     uid = session["devise.meetup_data"]["uid"]
-    response = RestClient.get "https://api.meetup.com/2/events?status=upcoming&order=time&limited_events=False&desc=false&member_id=10653533&offset=0&photo-host=public&format=json&page=20&fields=&sig_id=#{uid}&sig=594ee8c4bb58d735c6ab32c828210391680658e5"
+    puts "*" * 100
+    puts uid
+    puts "*" * 100
+    response = RestClient.get "https://api.meetup.com/2/events?member_id=#{uid}&offset=0&format=json&limited_events=False&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=10653533&sig=594ee8c4bb58d735c6ab32c828210391680658e5"
     puts "*" * 50
     puts response
 
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,8 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def meetup
     # You need to implement the method below in your model (e.g. app/models/user.rb)
+    puts "entering omnioath controller!\n"
+
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted? #remember me on their side?

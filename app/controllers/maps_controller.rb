c29f8a70c533c49cd9a6260b0dbb017025b5ccbe
Irina Tagintseva <irinatag@gmail.com>
2015-03-20 09:50:47 -0700

    pass sig parameter to api call

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,10 +5,14 @@ class MapsController < ApplicationController
   def show
     puts "session hash:" + session["devise.meetup_data"].to_s
     uid = session["devise.meetup_data"]["uid"]
+    sig = session["devise.meetup_data"]["sig"]
     puts "*" * 100
     puts uid
     puts "*" * 100
-    response = RestClient.get "https://api.meetup.com/2/events?member_id=#{uid}&offset=0&format=json&limited_events=False&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=10653533&sig=594ee8c4bb58d735c6ab32c828210391680658e5"
+    puts "*" * 100
+    puts sig
+    puts "*" * 100
+    response = RestClient.get "https://api.meetup.com/2/events?member_id=#{uid}&offset=0&format=json&limited_events=False&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=#{uid}&sig=594ee8c4bb58d735c6ab32c828210391680658e5"
     puts "*" * 50
     puts response
 
